cf36511efc32cd7000200f822d11ab64cf269796
Thurzas <mathieu.miot@gmail.com>
2025-02-07 13:46:51 +0100

    update css

--- client/src/pages/RaidHistory.tsx
+++ client/src/pages/RaidHistory.tsx
@@ -20,15 +20,20 @@ function RaidHistory() {
       <NavBar />
       <div className="questPanel">
         <svg width="0" height="0">
-            <filter id="crumpledPaper">
-              <feTurbulence type="fractalNoise" baseFrequency="0.01" numOctaves="8" />
-              <feComponentTransfer in="turbulence" result="grayNoise">
-                <feFuncR type="linear" slope="0.3" intercept="0.4"/>
-                <feFuncG type="linear" slope="0.3" intercept="0.4"/>
-                <feFuncB type="linear" slope="0.3" intercept="0.4"/>
-              </feComponentTransfer>
-              <feDisplacementMap in="SourceGraphic" scale="10" />
-            </filter>
+          <title id="filterTitle">Effet de papier froissé avec ombre</title>
+          <filter id="crumpledPaper">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.01"
+              numOctaves="8"
+            />
+            <feComponentTransfer in="turbulence" result="grayNoise">
+              <feFuncR type="linear" slope="0.3" intercept="0.4" />
+              <feFuncG type="linear" slope="0.3" intercept="0.4" />
+              <feFuncB type="linear" slope="0.3" intercept="0.4" />
+            </feComponentTransfer>
+            <feDisplacementMap in="SourceGraphic" scale="10" />
+          </filter>
         </svg>
         <div className="liegePart">
           <hr color="black" />
--- client/src/pages/raidDetail.tsx
+++ client/src/pages/raidDetail.tsx
@@ -28,6 +28,22 @@ function RaidDetail() {
     return (
       <>
         <NavBar />
+        <svg width="0" height="0">
+          <title id="filterTitle">Effet de papier froissé avec ombre</title>
+          <filter id="crumpledPaper">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.01"
+              numOctaves="8"
+            />
+            <feComponentTransfer in="turbulence" result="grayNoise">
+              <feFuncR type="linear" slope="0.3" intercept="0.4" />
+              <feFuncG type="linear" slope="0.3" intercept="0.4" />
+              <feFuncB type="linear" slope="0.3" intercept="0.4" />
+            </feComponentTransfer>
+            <feDisplacementMap in="SourceGraphic" scale="10" />
+          </filter>
+        </svg>
         <div className="questPanel">
           <div className="liegePart">
             <hr color="black" />
@@ -46,6 +62,22 @@ function RaidDetail() {
   return (
     <>
       <NavBar />
+      <svg width="0" height="0">
+        <title id="filterTitle">Effet de papier froissé avec ombre</title>
+        <filter id="crumpledPaper">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.01"
+            numOctaves="8"
+          />
+          <feComponentTransfer in="turbulence" result="grayNoise">
+            <feFuncR type="linear" slope="0.3" intercept="0.4" />
+            <feFuncG type="linear" slope="0.3" intercept="0.4" />
+            <feFuncB type="linear" slope="0.3" intercept="0.4" />
+          </feComponentTransfer>
+          <feDisplacementMap in="SourceGraphic" scale="10" />
+        </filter>
+      </svg>
       <div className="questPanel">
         <div className="liegePart">
           <hr color="black" />
